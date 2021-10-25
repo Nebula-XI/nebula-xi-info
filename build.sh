e741cad98611aad01e8a4bc0cc02b94de737ed8f
Alexander Chernenko <chernenko.a@insys.ru>
2021-10-25 17:35:54 +0300

    refactoring

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 build_presets=(linux-x64-release linux-x64-debug windows-x64-release windows-x64-debug)
 test_presets=(linux-x64-release linux-x64-debug)
 
-for arg in "$*"; do
+for arg in "$@"; do
     if [[ "$arg" == "config" ]]; then
         for i in ${build_presets[@]}; do
             echo "-- Configure $i"
