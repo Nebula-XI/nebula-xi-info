e777442f965eb4326068731e5496565e9ac3a39f
Alexander Chernenko <chernenko.a@insys.ru>
2021-10-28 19:29:39 +0300

    Removed cross-mingw presets

--- make.sh
+++ make.sh
@@ -11,7 +11,7 @@ function usage(){
     echo -e '\t' "help -  Show this usage."
 }
 
-build_presets=(linux-x64-release linux-x64-debug windows-x64-release windows-x64-debug)
+build_presets=(linux-x64-release linux-x64-debug)
 test_presets=(linux-x64-release linux-x64-debug)
 
 if [[ "$#" == 0 ]]; then
